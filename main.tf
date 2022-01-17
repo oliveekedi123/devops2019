ed4acd905c2ff8c26fe24011c2d5469c8ccc55f1
oliveekedi123 <oliveekedi@ymail.com>
2022-01-16 21:04:35 -0600

    main commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+resources:
+  - name: test-shared-vpc-subnet-iam-policy
+    type: shared_vpc_subnet_iam.py
+    properties:
+      policy:
+        bindings:
+          - resourceId: test-subnet-1
+            region: us-east1
+            role: roles/compute.networkUser
+            members:
+              - user:name@example.com
+              - serviceAccount:example@myprojectname.gserviceaccount.com
+          - resourceId: <FIXME:subnetId>
+            region: us-east1
+            role: roles/compute.networkUser
+            members:
+              - group:admins@example.com
+              - domain:example.com
